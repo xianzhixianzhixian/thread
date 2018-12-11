f6792bdce6f31c983ecb28c70e0a1d1c20774452
xianzhixianzhixian <MrRobotFan@163.com>
2018-12-11 20:36:41 +0800

    suspend()和resume()方法线程独占的两种情况以及原因

--- src/thread/suspend/resume/deallock/MyThread.java
+++ src/thread/suspend/resume/deallock/MyThread.java
@@ -7,6 +7,13 @@ package thread.suspend.resume.deallock;
  */
 public class MyThread extends Thread {
 
+    /**
+     * 原因：
+     * 每当调用System.out.println()方法时，该方法会获得this也就是PrintStream对象的锁(System.out)
+     * 在本例中run()方法中含有System.out.println()方法
+     * 当调用thread.suspend()方法时该线程暂停并且未释放PrintStream对象锁
+     * 导致main线程无法获得PrintStream对象锁，一直无法输出"main"字符串
+     */
     private long i=0;
     @Override
     public void run() {
