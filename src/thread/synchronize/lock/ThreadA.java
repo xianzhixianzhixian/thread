03ff063f0b03ee74c554174bdb8097401db1f207
xianzhixianzhixian <MrRobotFan@163.com>
2018-12-13 22:32:39 +0800

    验证多个线程调用一个类的同一个实例时synchronized修饰的方法和非synchronized修饰的方法的同步性

---
+++ src/thread/synchronize/lock/ThreadA.java
@@ -0,0 +1,20 @@
+package thread.synchronize.lock;
+
+/**
+ * @author: xianzhixianzhixian
+ * @date: 2018-12-13 22:13
+ */
+public class ThreadA extends Thread {
+
+    private MyObject myObject;
+
+    public ThreadA(MyObject object){
+        this.myObject = object;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        myObject.methodA();
+    }
+}
