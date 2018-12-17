105d8581a5d28d56ebcde085ca9c271ab9cb2d50
xianzhixianzhixian <MrRobotFan@163.com>
2018-12-17 21:37:45 +0800

    测试在父子类继承中也支持可重入锁

---
+++ src/thread/sync/lock/in/Run.java
@@ -0,0 +1,13 @@
+package thread.sync.lock.in;
+
+/**
+ * @author: xianzhixianzhixian
+ * @date: 2018-12-17 20:10
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        MyThread t= new MyThread();
+        t.start();
+    }
+}
