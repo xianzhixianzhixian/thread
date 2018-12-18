b131feff42cc6bc7ffd1dd27c71e2cbd312938f4
xianzhixianzhixian <MrRobotFan@163.com>
2018-12-18 20:44:42 +0800

    String的常量池特性和对线程运行的影响

---
+++ src/thread/synchronize/string/Run.java
@@ -0,0 +1,21 @@
+package thread.synchronize.string;
+
+/**
+ * @author: xianzhixianzhixian
+ * @date: 2018-12-18 20:35
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        StringTest testStr = new StringTest();
+        ThreadA threadA = new ThreadA(testStr);
+        threadA.setName("a");
+        ThreadB threadB = new ThreadB(testStr);
+        threadB.setName("b");
+        ThreadC threadC = new ThreadC(testStr);
+        threadC.setName("c");
+        threadA.start();
+        threadB.start();
+        threadC.start();
+    }
+}
---
+++ src/thread/synchronize/string/StringTest.java
@@ -0,0 +1,21 @@
+package thread.synchronize.string;
+
+/**
+ * 探究String的常量池特性在多线程中的影响
+ * @author: xianzhixianzhixian
+ * @date: 2018-12-18 20:32
+ */
+public class StringTest {
+
+    public void testStringSync(String str){
+        synchronized(str){
+            try {
+                System.out.println("线程"+Thread.currentThread().getName()+"运行开始");
+                Thread.sleep(1000);
+                System.out.println("线程"+Thread.currentThread().getName()+"运行结束");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
