979cb5db5b738ec0596292f5ae3026667bd30ffd
xianzhixianzhixian <MrRobotFan@163.com>
2019-01-14 21:37:54 +0800

    ReentrantLock和Condition实现多生产者多消费者交替打印值

---
+++ src/lock/condition/producercustomer/manytomany/MyService.java
@@ -0,0 +1,50 @@
+package lock.condition.producercustomer.manytomany;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 用await()和signalAll()实现多生产者多消费者交替打印
+ * while(){}循环中的字符串可能连续打印多次原因是：signalAll()唤醒的有可能是同类线程，如果使用多个condition则不会出现这种情况了
+ * @author: xianzhixianzhixian
+ * @date: 2019-01-14 20:34
+ */
+public class MyService {
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition condition0 = lock.newCondition(); //生产者condition
+    private Condition condition1 = lock.newCondition(); //消费者condition
+    private Boolean hasValue = false;
+    public void set(){
+        try {
+            lock.lock();
+            while (hasValue){ //这里一定要用while
+                System.out.println(Thread.currentThread().getName()+"生产者等待设置值有可能连续打印多次");
+                condition0.await();
+            }
+            System.out.println(Thread.currentThread().getName()+"设置值");
+            hasValue = true;
+            condition0.signalAll(); //这里一定要用signalALL()，避免出现多生产者/多消费者的假死情况
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void get(){
+        try {
+            lock.lock();
+            while (!hasValue){ //这里一定要用while
+                System.out.println(Thread.currentThread().getName()+"消费者等待设置值有可能连续打印多次");
+                condition0.await();
+            }
+            System.out.println(Thread.currentThread().getName()+"获取值");
+            hasValue = false;
+            condition0.signal(); //这里一定要用signalALL()，避免出现多生产者/多消费者的假死情况
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
---
+++ src/lock/condition/producercustomer/manytomany/Run.java
@@ -0,0 +1,28 @@
+package lock.condition.producercustomer.manytomany;
+
+/**
+ * @author: xianzhixianzhixian
+ * @date: 2019-01-14 20:43
+ */
+public class Run {
+    public static void main(String[] args) {
+        /*MyService myService = new MyService();
+        ThreadA[] a = new ThreadA[10];
+        ThreadB[] b = new ThreadB[10];
+        for (int i = 0; i < 10; i++) {
+            a[i] = new ThreadA(myService);
+            a[i].start();
+            b[i] = new ThreadB(myService);
+            b[i].start();
+        }*/
+        MyService myService = new MyService();
+        ThreadA[] a = new ThreadA[10];
+        ThreadB[] b = new ThreadB[10];
+        for (int i = 0; i < 10; i++) {
+            a[i] = new ThreadA(myService);
+            a[i].start();
+            b[i] = new ThreadB(myService);
+            b[i].start();
+        }
+    }
+}
