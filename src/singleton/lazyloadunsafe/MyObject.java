ac040c7b34575fc309c2ec521ac977e9c553438d
xianzhixianzhixian <MrRobotFan@163.com>
2019-02-21 23:11:51 +0800

    单例模式延迟加载创建出多个不同对象的情况

---
+++ src/singleton/lazyloadunsafe/MyObject.java
@@ -0,0 +1,26 @@
+package singleton.lazyloadunsafe;
+
+/**
+ * 演示懒汉模式时多线程会创建多个对象的情况
+ * @author: xianzhixianzhixian
+ * @date: 2019-02-21 23:00
+ */
+public class MyObject {
+    private static MyObject myObject;
+
+    public MyObject() {
+    }
+    public static MyObject getInstance() {
+        try {
+            if (myObject != null) {
+            } else {
+                //模拟在创建对象之前做一些准备性工作
+                Thread.sleep(3000);
+                myObject = new MyObject();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return myObject;
+    }
+}
