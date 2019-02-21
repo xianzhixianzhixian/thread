f579fdf2dcfe5e117b9925ef2671b374922d85c1
xianzhixianzhixian <MrRobotFan@163.com>
2019-02-21 23:13:09 +0800

    单例模式延迟加载通过同方法实现只创建一个对象的情况(效率低)

---
+++ src/singleton/lazyloadsafe/synchronizedmethod/MyThread.java
@@ -0,0 +1,12 @@
+package singleton.lazyloadsafe.synchronizedmethod;
+
+/**
+ * @author: xianzhixianzhixian
+ * @date: 2019-02-21 23:03
+ */
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println(MyObject.getInstance().hashCode());
+    }
+}
